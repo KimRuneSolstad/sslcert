a820ca62857db2eb80a388f0a856af3c2fbfce6a
Kim Rune Solstad <kimrunesolstad89@gmail.com>
2016-09-06 00:05:48 +0200

    commit before revert

--- spec/acceptance/standard_spec.rb
+++ spec/acceptance/standard_spec.rb
@@ -14,11 +14,17 @@ describe 'sslcert class' do
 #      command => 'openssl version',
 #      path    => '/etc/ssl/',
 #    }
-
     EOS
+
 
     # Run it twice and test for idempotency
     expect(apply_manifest(pp).exit_code).to_not eq(1)
     expect(apply_manifest(pp).exit_code).to eq(0)
+
+#    context 'managing CA' do
+#      describe file('/etc/ssl/CA/serial') do
+#        it { should be_file }
+#      end
+#    end
   end
 end
